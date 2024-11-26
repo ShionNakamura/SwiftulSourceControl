c1b9b596e11c8bdf527cf4404ee2b16445ce70fc
ShionNakamura <shionnakamura0120@gmail.com>
2024-11-26 16:13:10 +0900

    oooo

---
+++ SwiftulSourceControl/HomeView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeView.swift
+//  SwiftulSourceControl
+//
+//  Created by 仲村士苑 on 2024/11/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var title: String = "Hello World"
+    var body: some View {
+        Text("Home Screen")
+        Text("Hi")
+        Text("Screen 2")
+        Text("Screen 3")
+        Text("Screen 4")
+        Text("Screen 5")
+        Text("Screen 6")
+
+
+    }
+}
+
+#Preview {
+    HomeView()
+}
