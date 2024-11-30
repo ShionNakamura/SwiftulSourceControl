19966102299494a15aea3c2e40b16878e5445d3e
ShionNakamura <shionnakamura0120@gmail.com>
2024-11-30 16:40:14 +0900

    [feature] Add home analytics

--- SwiftulSourceControl/HomeView.swift
+++ SwiftulSourceControl/HomeView.swift
@@ -9,18 +9,24 @@ import SwiftUI
 
 struct HomeView: View {
     @State private var title: String = "Hello World"
+    
     var body: some View {
-        Text("Home Screen")
-        Text("Hi")
-        Text("Screen 2")
-        Text("Screen 3")
-        Text("Screen 4")
-        Text("Screen 5")
-        Text("Screen 6")
-
-
+        VStack { // Use a VStack to stack the Text views vertically
+            Text("Home Screen")
+            Text("Hi")
+            Text("Screen 2")
+            Text("Screen 3")
+            Text("Screen 4")
+            Text("Screen 5")
+            Text("Screen 6")
+        }
+        .onAppear {
+            
+        }
     }
-}
+  }
+
+
 
 #Preview {
     HomeView()
